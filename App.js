84a72edd4a4fe5f7c88dd7f5b2a3a8be42153606
Kajal Gangdhar Potdar <93221887+Kajalg29@users.noreply.github.com>
2022-01-17 18:25:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import "./index.css";
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+import { useState } from 'react';
+
+
+function App() {
+  const [usersList,setUsersList]=useState([]);
+ 
+  const addUserHandler=(uName,uAge)=>{
+    setUsersList((prevUsersList)=>{
+      return [...prevUsersList, {name:uName, age:uAge, id:Math.random().toString}];
+    });
+  }
+  return (
+    <div className="App">
+     <AddUser onAddUser={addUserHandler}></AddUser>
+     <UsersList users={usersList}></UsersList>
+     
+    </div>
+  );
+}
+
+export default App;
